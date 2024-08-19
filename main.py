378bfc55b234a2eb893e78416f70fc1faa3f09d0
RomainDussuchal <romain-vrb@hotmail.fr>
2024-08-19 17:22:02 +0200

    launching pygame

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    run = True
+
+    while run :
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+       
+        pygame.display.flip()
+    
+    pygame.quit()
+    exit()
+        
+        
+
+if __name__ == "__main__":
+    main()
